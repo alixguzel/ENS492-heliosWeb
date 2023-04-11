a2b61ba7b4c31d834804ef8bcf4a7b223debde5b
Ali Güzel <aliguzel.dev@gmail.com>
2023-04-12 00:59:58 +0300

    Update network.js

--- network.js
+++ network.js
@@ -1,42 +1,53 @@
 import { Helios } from "https://cdn.skypack.dev/helios-web?min";
 
-const fs = require('fs');
-const zlib = require('zlib');
-
+// Nodes are dictionaries (any key can be used as node properties)
 const nodes = {};
 
-fs.createReadStream('file.jsons.gz')
-  .pipe(zlib.createGunzip())
-  .on('data', (data) => {
-    const json = JSON.parse(data.toString());
-    nodes[parseInt(json.id_str)] = json;
-  })
-  .on('end', () => {
-    console.log(nodes);
-  });
+fetch('dataverse_files/networks/nodes/follower_profiles_2022-09.jsons.gz')
+  .then(response => response.blob())
+  .then(blob => {
+    const reader = new FileReader();
+    reader.onload = function() {
+      const nodes = {};
+      const jsons = JSON.parse(reader.result);
 
-// Nodes are dictionaries (any key can be used as node properties)
+      for (let i = 0; i < jsons.length; i++) {
+        nodes[jsons[i].id_str] = jsons[i];
+      }
+
+      console.log(nodes);
+    };
+    reader.readAsText(blob);
+  });
 
 
 // Edges are arrays of node ids
-let edges = [
-{
-    source: "0",
-    target: "1"
-},
-{
-    source: "1",
-    target: "2"
-},
-{
-    source: "2",
-    target: "0"
-},
-{
-    source: "2",
-    target: "3"
-}
-];
+const edges = [];
+
+fetch('dataverse_files/networks/edges')
+  .then(response => response.json())
+  .then(jsons => {
+    jsons.filter((filename) => filename.endsWith('.jsons.gz'))
+      .forEach((filename) => {
+        const id = filename.match(/(\d+)_network.jsons.gz/)[1];
+        fetch(`edges/${filename}`)
+          .then(response => response.blob())
+          .then(blob => {
+            const reader = new FileReader();
+            reader.onload = function() {
+              const json = JSON.parse(zlib.gunzipSync(reader.result).toString());
+              json.friends.forEach((friend) => {
+                edges.push({ source: friend, target: id });
+              });
+            };
+            reader.readAsText(blob);
+          });
+      });
+  });
+
+console.log(edges);
+
+
 
 let helios = new Helios({
 elementID: "netviz", // ID of the element to render the network in
